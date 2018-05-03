c494f5918a3703395d98bf624012c37bd08493f4
Gytautas <gytautas.pilkauskas@gmail.com>
2018-05-03 14:29:00 +0300

    Update contact_us.php

--- views/contact_us.php
+++ views/contact_us.php
@@ -1,3 +1,48 @@
-<?php 
-// Implementation
-?>
+<?php include ('header.php') ?>
+
+<div class="ui middle aligned center aligned grid">
+  <div class="column">
+    <h2 class="ui image header">
+      <div class="content">
+        Registering with VEX... <br/> <span class="small-text">...is a smart move</span>
+      </div>
+    </h2>
+    <form action="/contact.php" method="post" class="ui large form">
+        <?= error() ?>
+      <div class="ui stacked segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="name" placeholder="Name">
+            </div>
+          </div>
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input type="text" name="email" placeholder="E-mail address">
+          </div>
+        </div>
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input type="text" name="phone" placeholder="Phone">
+          </div>
+        </div>
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <textarea rows="2" name="message" placeholder="Message"></textarea>
+          </div>
+        </div>
+        <button class="ui fluid large primary submit button">Login</button>
+      </div>
+
+      <div class="ui error message"></div>
+
+    </form>
+
+  </div>
+</div>
+
+
+<?php include ('footer.php') ?>
